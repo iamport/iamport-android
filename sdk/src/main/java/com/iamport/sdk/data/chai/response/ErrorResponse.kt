ebba541da5b7d421b3dac20018c1be6678c1d241
bingbong <akrasias2@naver.com>
2020-12-22 14:40:45 +0900

    fix : ErrorResponse type 파라미터 추가

--- sdk/src/main/java/com/iamport/sdk/data/chai/response/ErrorResponse.kt
+++ sdk/src/main/java/com/iamport/sdk/data/chai/response/ErrorResponse.kt
@@ -1,3 +1,3 @@
 package com.iamport.sdk.data.chai.response
 
-data class ErrorResponse(val error: String, val message: String?, val code: Int?)
+data class ErrorResponse(val error: String?, val type: String?, val message: String?, val code: String?)
