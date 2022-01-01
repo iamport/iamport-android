6b20fca2d3983df085f7094768d9ebaa9670d769
bingbong <akrasias2@naver.com>
2022-01-01 21:53:15 +0900

    fix : custom_data base64 en/decode 처리

--- sdk/src/main/java/com/iamport/sdk/domain/JsNativeInterface.kt
+++ sdk/src/main/java/com/iamport/sdk/domain/JsNativeInterface.kt
@@ -1,5 +1,6 @@
 package com.iamport.sdk.domain
 
+import android.util.Base64
 import android.webkit.JavascriptInterface
 import com.google.gson.Gson
 import com.iamport.sdk.data.sdk.IamPortCertification
@@ -76,10 +77,10 @@ class JsNativeInterface(val payment: Payment, val gson: Gson, val evaluateJS: ((
     }
 
     private fun requestPay(request: IamPortRequest) {
-        if(request.custom_data.isNullOrEmpty()) {
+        if (request.custom_data.isNullOrEmpty()) {
             requestPayNormal(request)
         } else {
-            requestPayWithCustomData(request)
+            requestPayWithCustomData(request, request.custom_data)
         }
     }
 
@@ -88,9 +89,11 @@ class JsNativeInterface(val payment: Payment, val gson: Gson, val evaluateJS: ((
         evaluateJS("requestPay('${gson.toJson(request)}');")
     }
 
-    private fun requestPayWithCustomData(request: IamPortRequest) {
+    private fun requestPayWithCustomData(request: IamPortRequest, customData: String) {
         Logger.d(request)
-        evaluateJS("requestPayWithCustomData('${gson.toJson(request)}', '${request.custom_data}');")
+
+        val encodedString: String = Base64.encodeToString(customData.toByteArray(), Base64.NO_WRAP)
+        evaluateJS("requestPayWithCustomData('${gson.toJson(request)}', '${encodedString}');")
     }
 
     private fun certification(certification: IamPortCertification) {
